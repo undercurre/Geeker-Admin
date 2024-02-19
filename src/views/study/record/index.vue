b01f1f777f59670e312fd4802767b725b9d9d0e0
lirh42 <undercurre@163.com>
2024-02-19 11:35:11 +0800

    feat(component): 🚀 序号加1

--- src/views/study/record/index.vue
+++ src/views/study/record/index.vue
@@ -11,7 +11,7 @@
         <el-table-column label="Index">
           <template #default="scope">
             <div style="display: flex; align-items: center">
-              <span>{{ scope.$index }}</span>
+              <span>{{ scope.$index + 1 }}</span>
             </div>
           </template>
         </el-table-column>
